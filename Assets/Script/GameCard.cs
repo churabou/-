6d9664bb485a58a26edd979895c7d77a00070ff1
chutatsu <chuutatsu@chutatsu.local>
2017-07-31 17:56:35 +0900

    initial commit

---
+++ Assets/Script/GameCard.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameCard : MonoBehaviour {
+
+
+
+	private string id;
+	public GameObject card;
+	private Button button {
+		get{
+			return card.GetComponent<Button>();
+		 }
+	}
+		
+	// Use this for initialization
+	void Start () {
+
+	}
+
+
+
+	public void onClickCard(){
+		StatusManager.count += 1;
+		StatusManager.selectedCards.Add (this);
+		button.GetComponentInChildren<Image>().color = Color.green;
+		iTween.RotateTo (card, iTween.Hash ("y", 180, "oncomplete", "OnRotateComplete",
+			"isLocal", true));
+
+	}
+
+	private void OnRotateComplete(){
+
+
+		if (StatusManager.count == 2) {
+			for (var i=0; i<2; i++){
+
+			GameCard gc = StatusManager.selectedCards [i];
+             
+			gc.button.GetComponentInChildren<Image>().color = Color.red;
+			iTween.RotateTo (gc.card, iTween.Hash ("y", 0, "oncomplete", "OnRotateComplete",
+				"isLocal", true));
+			}
+			StatusReset ();
+		}
+
+	}
+
+
+	private void StatusReset(){
+		StatusManager.count = 0;
+		StatusManager.selectedCards = new List<GameCard>();
+	}
+
+
+}
---
+++ Assets/Script/StatusManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class StatusManager {
+
+
+	public static int count = 0;
+
+	public static List<GameCard> selectedCards = new List<GameCard>();
+
+
+}
